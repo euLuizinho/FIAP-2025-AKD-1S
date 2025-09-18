340a810f2d73935a64c13e8a309e59abd3c8eef1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 21:05:16 -0300

    Continuação do App GoldWise

--- GoldWise/app/src/main/java/br/com/luizinho/goldwise/MainActivity.kt
+++ GoldWise/app/src/main/java/br/com/luizinho/goldwise/MainActivity.kt
@@ -9,19 +9,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +56,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GoldWise(modifier: Modifier = Modifier) {
+
+    var quantity by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -65,6 +83,63 @@ fun GoldWise(modifier: Modifier = Modifier) {
                 fontWeight =  FontWeight.Bold
             )
         }
+
+        Spacer(modifier = Modifier.height(24.dp))
+        
+        OutlinedTextField(
+            value = quantity,
+            onValueChange = { quantity = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Quantidade") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = price,
+            onValueChange = { price = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Preço") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        var context = LocalContext.current
+
+        Button(
+            onClick = {
+                if (price.isEmpty() || quantity.isEmpty()){
+                    result = "Preencha todos os campos!"
+                }else{
+                    val total = quantity.toDouble()*price.toDouble()
+                    result = context.getString(R.string.label_result, total)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Calcular")
+        }
+
+        OutlinedButton(
+            onClick = {
+                quantity = ""
+                price = ""
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Limpar")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(result)
+
 
     }
 }
