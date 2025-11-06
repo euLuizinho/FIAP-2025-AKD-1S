79c373245eec456d7e7eeb5226b24c21963067de
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-05 22:28:35 -0300

    Adição de mais um pŕojeto feito em aula

---
+++ LudoOn/app/src/main/java/com/example/luizinho/ludoon/models/Story.kt
@@ -0,0 +1,6 @@
+package com.example.luizinho.ludoon.models
+
+data class Story (
+    val userNickName: String,
+    val userAvatar: String
+)
---
+++ LudoOn/app/src/main/java/com/example/luizinho/ludoon/ui/screens/HomeScreen.kt
@@ -0,0 +1,41 @@
+package com.example.luizinho.ludoon.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.luizinho.ludoon.data.repository.stories
+import com.example.luizinho.ludoon.ui.components.LudoOnTopBar
+import com.example.luizinho.ludoon.ui.components.StoryList
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = { LudoOnTopBar() },
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+
+        ) {
+
+            StoryList(stories)
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(modifier: Modifier = Modifier) {
+    HomeScreen()
+}
